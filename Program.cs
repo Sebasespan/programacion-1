4f34f4f18306964948394bed14e71b851316c689
Sebasespan <92902333+Sebasespan@users.noreply.github.com>
2021-10-20 23:18:23 -0400

    Ejercicio práctico C# - Primer Corte
    Para este ejercicio tendrán que mostrar un número de tres dígitos formado por tres variables numéricas en diferentes combinaciones. 
    
    Por ejemplo la combinación "abc" -> 241  
    
    Para este caso las variables serian: 
    
    "a" sería el número 2
    "b" sería el número 4
    "c" sería el número 1
    
    Has un programa que produzca números aacb, bca, bcbc. Puedes utilizar los números que prefieras.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace C_
+{
+   class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ejercicio de variable tipo aabc");
+            int a = 2;
+            int b = 4;
+            int c = 7;
+
+            Console.Write(a);
+            Console.Write(a);
+            Console.Write(c);
+            Console.WriteLine(b);
+
+            Console.WriteLine("Ejercicio de variable tipo bca");
+
+            Console.Write(b);
+            Console.Write(c);
+            Console.WriteLine(a);
+
+            Console.WriteLine("Ejercicio de variable tipo bcbc");
+
+            Console.Write(b);
+            Console.Write(c);
+            Console.Write(b);
+            Console.WriteLine(c);
+
+            Console.WriteLine("Listo. Espero mi nota :D");
+            
+
+
+
+
+
+
+        }
+    }
+
+}
+    
+
